3000f2d793770233aa27dc1c7ca8a8769c506131
morris__liu <morris46202@gmail.com>
2024-06-02 11:23:35 +0800

    fix: fix some warning about the type of input , and this commit means i gave up virtial box. It's so slow:(

--- hw0401.c
+++ hw0401.c
@@ -28,7 +28,7 @@ typedef struct return_data{
 
 void print_usage();
 void show_proc();
-rd get_cpu_data( int32_t pid );
+rd *get_cpu_data( int32_t pid );
 int find_pid( int32_t pid );
 
 int main( int argc , char *argv[] )
@@ -92,7 +92,7 @@ int main( int argc , char *argv[] )
         }
     }
     
-    rd cpu_data;
+    rd *cpu_data = malloc(sizeof(rd));
     if(p_flag == 1)
     {
         cpu_data = get_cpu_data(pid);
@@ -107,13 +107,13 @@ int main( int argc , char *argv[] )
         printf("PID   NAME      state   CPU     MEM\n");
         if(p_flag == 1)
         {
-            printf("pid: %d\n", cpu_data.pid);
-            printf("state: %c\n", cpu_data.state);
-            printf("utime: %lu\n", cpu_data.utime);
-            printf("stime: %lu\n", cpu_data.stime);
-            printf("cutime: %ld\n", cpu_data.cutime);
-            printf("cstime: %ld\n", cpu_data.cstime);
-            printf("vsize: %ld\n", cpu_data.vsize);
+            printf("pid: %d\n", cpu_data->pid);
+            printf("state: %c\n", cpu_data->state);
+            printf("utime: %lu\n", cpu_data->utime);
+            printf("stime: %lu\n", cpu_data->stime);
+            printf("cutime: %ld\n", cpu_data->cutime);
+            printf("cstime: %ld\n", cpu_data->cstime);
+            printf("vsize: %u\n", cpu_data->vsize);
         }
         else
         {
@@ -126,11 +126,13 @@ int main( int argc , char *argv[] )
             {
                 if(ptr->d_name[0] >= '0' && ptr->d_name[0] <= '9')
                 {
+                    XD printf("pid: %d\n", atoi(ptr->d_name));
                     cpu_data = get_cpu_data(atoi(ptr->d_name));
-                    double all_time = cpu_data.utime + cpu_data.stime + cpu_data.cutime + cpu_data.cstime;
-                    double sec = (cpu_data.uptime - (cpu_data.starttime / HZ)) ;
+                    HI
+                    double all_time = cpu_data->utime + cpu_data->stime + cpu_data->cutime + cpu_data->cstime;
+                    double sec = (cpu_data->uptime - (cpu_data->starttime / HZ)) ;
                     double cpu_usage = ((all_time / HZ) / sec) *100;
-                    printf("%d     %s      %c      %lu     %ld\n", cpu_data.pid, ptr->d_name, cpu_data.state, cpu_usage, cpu_data.vsize);
+                    printf("%d     %s      %c      %f     %f\n", cpu_data->pid, ptr->d_name, cpu_data->state, cpu_usage, cpu_data->vsize/1024.0);
                 }
             }
             closedir(dir);
@@ -145,7 +147,7 @@ int main( int argc , char *argv[] )
 
 void print_usage()
 {
-    printf("2 Usage: hw0401 [options]\n");
+    printf("Usage: hw0401 [options]\n");
     printf(" -t, --time-interval=time Update the information every [time] seconds. Default: 5 seconds.\n");
     printf(" -c, --count=count Update the information [count] times. Default: infinite.\n");
     printf(" -p, --pid=pid Only display the given process information.\n");
@@ -167,7 +169,7 @@ void show_proc()
 
 
 // get some data in /proc
-rd get_cpu_data( int32_t mypid )
+rd *get_cpu_data( int32_t mypid )
 {
     char path[100];
     sprintf(path, "/proc/%d/stat",mypid);
@@ -175,7 +177,7 @@ rd get_cpu_data( int32_t mypid )
     if(fp == NULL)
     {
         printf("Error: cannot open file\n");
-        return;
+        return NULL;
     }
     
     // read file
@@ -183,11 +185,12 @@ rd get_cpu_data( int32_t mypid )
     char comm[100], state;
     unsigned long minflt, cminflt, majflt, cmajflt, utime, stime;
     int64_t cutime, cstime;
-    int32_t priority, nice, num_threads, itrealvalue;
-    u_int64_t starttime;
-    u_int32_t vsize;
+    int32_t priority, nice, num_threads;
+    int32_t itrealvalue;
+    unsigned long starttime;
+    unsigned int vsize;
     
-    fscanf(fp, "%d %s %c %d %d %d %d %d %u %lu %lu %lu %lu %lu %lu %ld %ld, %ld %ld %d %d %d %d %u %llu %lu",
+    fscanf(fp, "%d %s %c %d %d %d %d %d %u %lu %lu %lu %lu %lu %lu %ld %ld %d %d %d %d %ld %d %d %lu %u",
             &pid, comm, &state, &ppid, &pgrp, &session, &tty_nr, &tpgid, &flags,
             &minflt, &cminflt, &majflt, &cmajflt, &utime, &stime, &cutime, &cstime,
             &priority, &nice, &num_threads, &itrealvalue, &starttime, &vsize);
@@ -200,31 +203,31 @@ rd get_cpu_data( int32_t mypid )
     XD printf("stime: %lu\n", stime);
     XD printf("cutime: %ld\n", cutime);
     XD printf("cstime: %ld\n", cstime);
-    XD printf("starttime: %llu\n", starttime);
-    XD printf("vsize: %lu\n", vsize);
+    XD printf("starttime: %lu\n", starttime);
+    XD printf("vsize: %u\n", vsize);
 
     fclose(fp);
 
-    rd ret;
-    ret.pid = pid;
-    ret.state = state;
-    ret.utime = utime;
-    ret.stime = stime;
-    ret.cutime = cutime;
-    ret.cstime = cstime;
-    ret.starttime = starttime;
-    ret.vsize = vsize;
+    rd *ret = malloc(sizeof(rd));
+    ret->pid = pid;
+    ret->state = state;
+    ret->utime = utime;
+    ret->stime = stime;
+    ret->cutime = cutime;
+    ret->cstime = cstime;
+    ret->starttime = starttime;
+    ret->vsize = vsize;
 
     //get uptime
     fp = fopen("/proc/uptime", "r");
     if(fp == NULL)
     {
         printf("Error: cannot open file\n");
-        return;
+        return NULL;
     }
     double uptime;
     fscanf(fp, "%lf", &uptime);
-    ret.uptime = uptime;
+    ret->uptime = uptime;
 
     return ret;
 }
